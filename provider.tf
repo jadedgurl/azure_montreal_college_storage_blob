7dfb257a70be69205738ed5de2729dafe8f2cdfa
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-09-13 15:55:02 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,18 @@
-terraform{
-  required_providers{
-  azurerm={
-      source="hashicorp/azurerm"
-      version=">=3.59.0"#This is the version of Azure Rm module
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
-required_version=">=1.4.0"#This is the version of Terraform 
   }
 }
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
 provider "azurerm" {
     client_id=var.client_id
     client_secret=var.client_secret
